1b6fcb35b99cb3c167768ec2d3c5947af4982711
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-05-14 11:04:28 -0500

    actualizacion en pagina de inicio

---
+++ graficos.js
@@ -0,0 +1,135 @@
+(function () {
+
+    $(document).ready(function () {
+        grafico01();
+        grafico02();
+        grafico03();
+    });
+
+    function grafico01() {
+        // Crear el gráfico de barras
+        var ctx = document.getElementById('analisis-cv').getContext('2d');
+        var grafico = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Cantidad de productos por categoría'
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        stacked: true,
+                        grid: {
+                            display: false
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    function grafico02() {
+        // Crear el gráfico de barras
+        var ctx = document.getElementById('analisis-cv0').getContext('2d');
+        var grafico = new Chart(ctx, {
+            type: 'polarArea',
+            data: {
+                labels: ['Red', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Cantidad de productos por categoría'
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        stacked: true,
+                        grid: {
+                            display: false
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    function grafico03() {
+        // Crear el gráfico de barras
+        var ctx = document.getElementById('analisis-cv01').getContext('2d');
+        var grafico = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Cantidad de productos por categoría'
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        stacked: true,
+                        grid: {
+                            display: false
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+})();
